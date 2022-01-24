eadb189caf07252f3d75de15dc33d7e733e6bf94
LinKrik <leksin.m.s@gmail.com>
2022-01-24 13:36:23 +0400

    Главный класс каоманды для проекта

---
+++ Microwave equipment M2/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,26 @@
+﻿using Microwave_equipment_M2.Infrastructure.Commands.Base;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave_equipment_M2.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        LambdaCommand(Action<object> Execute, Predicate<object> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+    }
+}
